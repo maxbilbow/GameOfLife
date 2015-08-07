70398a6a102ba7367add64656b839088c5fef8a7
maxbilbow <maxbilbow@gmail.com>
2015-08-07 11:48:17 +0100

    setting up metal and OpenGL

---
+++ GameOfLife-XCode/GameOfLife-OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameOfLife-OpenGL
+//
+//  Created by Max Bilbow on 07/08/2015.
+//  Copyright © 2015 Max Bilbow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
